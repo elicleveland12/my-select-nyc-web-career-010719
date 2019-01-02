64c534e495e3b240fb090af30a7fb9a9294e3dd3
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 15:55:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,8 @@ def my_select(collection)
     i = 0
     new_collection = []
     while i < collection.length 
-      
+      if yield(collection[i]) == true 
+        
       end 
     end 
   else 
