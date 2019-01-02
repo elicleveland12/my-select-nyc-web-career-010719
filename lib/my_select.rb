4c059ea240bbc8407f5c771b17a6753297b00f63
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 15:55:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,6 +6,7 @@ def my_select(collection)
       if yield(collection[i]) == true 
         new_collection.push(collection[i])
       end 
+      i += 1 
     end 
   else 
     puts "Please use a block."
